a099ee683f7a8cc3911f28a16ae85caed10288b0
Julie Davison <davison.jules@gmail.com>
2019-06-06 16:54:56 -0700

    js starting code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,56 @@
-// Do I need this?
-// $(document).ready(function() {
-
-
 // <!-- Use key events to listen for the letters that your players will type. -->
+
 // Variables
 
+var possibleWords = ["list", "words", "here"];
 
 
+//Empty variables
+var guessingWord = [];
+var guessedLetters = [];
+// var currentStatus = guessingWord.slice();
+var wordToMatch
+var numGuess
+var wins = 0
+
+
+
+//Shows word with underscores instead of letters.
+for (i = 0; i < guessingWord.length; i++) {
+    currentStatus[i] = "_"
+    console.log(currentStatus[i]);
+}
+
+
+//onkeypress event goes here.  "KEY EVENT"
+// when a key is pressed start the game if the key is an uppercase alphabet character.
+document.onkeypress = function (e) {
+    if (isAlpha(e.key) && e.key.toUpperCase()){
+        checkForLetter(e.key)
+    }
+    else
+        //change to uppercase
+        checkForLetter(e.key);
+    
+
+}
+//Check if letter is in the word and then process 
+function checkForLetter(){
+    var guess = document.getElementById("guess").value;
+    var found = false;
+    for (i = 0; i < hiddenWord.length; i++) {
+        if (hiddenWord[i] == guess) {
+            // if the guess donst exist set a flag and check later
+            // 
+            currentStatus[i] = guess;
+            found = true;
+        }
+    }
+}
+
+
+
+
+// function updateDisplay() {
+    // document.getElementById("currentWord").innerText = guessingWord.join("");
+// }
