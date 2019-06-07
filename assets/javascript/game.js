9cf3f542330cadbe3a6501f69f7745b3e7b8c2b1
Julie Davison <davison.jules@gmail.com>
2019-06-07 14:09:05 -0700

    added notes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,8 @@
 
 // Variables
 
-var possibleWords = ["list", "words", "here"];
+var possibleWords = ["list", "words", "here", "please"];
 
-
-//Empty variables
 var guessingWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
 var guessedLetters = [];
 var currentStatus = [];
@@ -13,8 +11,8 @@ var wordToMatch
 var numGuess
 var wins = 0
 
-// resetGame(); need function to reset game.
-
+//reset game.
+// resetGame();
 
 //Shows word with underscores instead of letters.
 for (i = 0; i < guessingWord.length; i++) {
@@ -24,22 +22,23 @@ console.log(guessingWord);
 
 
 console.log(currentStatus.join(' '));
+
 var currentWordEl = document.getElementById('currentWord');
 currentWordEl.innerText = currentStatus.join(' ');
 
 
-var isAlpha = function(ch){
+var isAlpha = function (ch) {
     return /^[A-Z]$/i.test(ch);
-  }
+}
 
 //onkeypress event goes here.  "KEY EVENT"
 // when a key is pressed start the game if the key is an uppercase alphabet character.
 document.onkeypress = function (e) {
     console.log(isAlpha(e.key));
-    if (isAlpha(e.key)){
+    if (isAlpha(e.key)) {
         var letterInWord = checkForLetter(e.key);
         console.log(typeof letterInWord);
-        if(letterInWord){
+        if (letterInWord) {
             //Inside this b;lock we know the gues was in the word
             //we need to update teh underscores on screen
             console.log(currentStatus)
@@ -52,38 +51,68 @@ document.onkeypress = function (e) {
         }
 
         //Is teh game completed
-        if(currentStatus.indexOf('_') < 0){
+        if (currentStatus.indexOf('_') < 0) {
             //User has completed the word
             alert('you win');
         }
     }
     else {
         //You didnt give us good input
-        alert ("Need an actual letter!")
+        alert("Need an actual letter!")
     }
-       
-    
+
+
 
 }
 //Check if letter is in the word and then process 
-function checkForLetter(keyPressed){
+function checkForLetter(keyPressed) {
     //var guess = document.getElementById("guess").value;
     var found = false;
+
     for (i = 0; i < guessingWord.length; i++) {
         if (guessingWord[i].toLowerCase() === keyPressed.toLowerCase()) {
             // if the guess donst exist set a flag and check later
             // 
             currentStatus[i] = guessingWord[i];
             found = true;
+
+
         }
+
     }
 
     return found;
 }
 
 
+// function resetGame() {
+// numGuess = maxGuess
+// pauseGame = false
 
-//Need function to update display.
-// function updateDisplay() {
-    // document.getElementById("currentWord").innerText = guessingWord.join("");
+// Get a new word
+// wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].tolowerCase()
+// console.log(wordToMatch)
+
+// Reset word arrays
+// guessedLetters = []
+// guessingWord = []
+
+// Reset the guessed word
+// for (var i = 0, j = wordToMatch.length; i < j; i++) {
+// Put a space instead of an underscore between multi word "words"
+// if (wordToMatch[i] === " ") {
+// guessingWord.push(" ")
+// } else {
+// guessingWord.push("_")
 // }
+// }
+// updateDisplay();
+// }
+//Need function to update display.
+function updateDisplay() {
+    // document.getElementById("totalWins").innerText = wins;
+    document.getElementById("currentWord").innerText = guessingWord;
+    // document.getElementById("remainingGuesses").innerText = numGuess;
+    // document.getElementById("guessedLetters").innerText = guessedLetters.join(" ");
+}
+updateDisplay();
