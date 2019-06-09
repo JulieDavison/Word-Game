a3f2933c531b2b8dc8b9755d69787e501ba2292e
Julie Davison <davison.jules@gmail.com>
2019-06-09 15:03:38 -0700

    added css background and fonts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,17 +5,13 @@
 var possibleWords = ["Earth", "galaxy", "asteroid", "comet", "constellations", "Neptune", "nebula", "Saturn", "Jupiter"];
 
 var maxGuess = 10
-// var pauseGame = false
 
 var guessingWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
 var guessedLetters = [];
 var currentStatus = [];
-// var wordToMatch
-var numGuess
-var wins = 0
 
-//reset game.
-// resetGame();
+var wins = 0;
+var losses = 0;
 
 //Shows word with underscores instead of letters.
 for (i = 0; i < guessingWord.length; i++) {
@@ -35,7 +31,7 @@ var isAlpha = function (ch) {
     return /^[A-Z]$/i.test(ch);
 }
 
-// A key is pressed start the game if the key is an uppercase alphabet character.
+// A key is pressed start the game if the key is an alphabet character.
 document.onkeypress = function (e) {
     var keyValue = e.key;
     console.log(keyValue);
@@ -45,15 +41,10 @@ document.onkeypress = function (e) {
         console.log(typeof letterInWord);
 
         if (letterInWord === true) {
-            // letterInWord.push(e.key);
-
-
-            // document.getElementById("currentWord").innerText = letterInWord.join(" ");
-            //Inside this b;lock we know the gues was in the word
-            //we need to update teh underscores on screen
+            //Inside this block we know the gues was in the word
+            //Update the underscores on screen
             console.log(currentStatus)
             document.getElementById("currentWord").innerHTML = currentStatus.join("" + " ");
-
         }
         else {
             //Guess was not in the word
@@ -61,29 +52,35 @@ document.onkeypress = function (e) {
             maxGuess--;
             console.log(maxGuess);
             document.getElementById("remainingGuesses").innerText = maxGuess;
-
-
+            
             guessedLetters.push(keyValue);
             console.log(guessedLetters);
             document.getElementById("guessedLetters").innerText = guessedLetters.join(" ");
+            
+            if (maxGuess <= 0) {
+                alert("Game Over");
+                reset();
+            }
         }
 
-        //Is teh game completed
+        //Is the game completed
         if (currentStatus.indexOf('_') < 0) {
             //User has completed the word
-            alert('you win');
+
             wins++;
             document.getElementById("totalWins").innerText = wins;
             console.log(wins);
+            alert("you win");
+            reset();
+
+        } else {
+            losses++;
         }
     }
     else {
         //You didnt give us good input
         alert("Need an actual letter!")
     }
-
-
-
 }
 //Check if letter is in the word and then process 
 function checkForLetter(keyPressed) {
@@ -92,48 +89,27 @@ function checkForLetter(keyPressed) {
 
     for (i = 0; i < guessingWord.length; i++) {
         if (guessingWord[i].toLowerCase() === keyPressed.toLowerCase()) {
-            // if the guess donst exist set a flag and check later
-            // 
             currentStatus[i] = guessingWord[i];
             found = true;
-
-
         }
-
     }
-
     return found;
 }
 
+function reset() {
+    var possibleWords = ["Earth", "galaxy", "asteroid", "comet", "constellations", "Neptune", "nebula", "Saturn", "Jupiter"];
+    maxGuess = 10;
+    numGuess = maxGuess;
+    guessingWord = possibleWords[Math.floor(Math.random() * possibleWords.length)];
+    guessedLetters = [];
+    currentStatus = [];
+    console.log(guessingWord);
+    document.getElementById("currentWord").innerHTML = currentStatus.join("" + " ");
+    for (i = 0; i < guessingWord.length; i++) {
+        currentStatus[i] = "_";
+    }
+    document.getElementById("remainingGuesses").innerText = maxGuess;
+    currentWordEl = document.getElementById('currentWord');
+    currentWordEl.innerText = currentStatus.join(' ');
 
-// function resetGame() {
-// numGuess = maxGuess
-// pauseGame = false
-
-// Get a new word
-// wordToMatch = possibleWords[Math.floor(Math.random() * possibleWords.length)].tolowerCase()
-// console.log(wordToMatch)
-
-// Reset word arrays
-// guessedLetters = []
-// guessingWord = []
-
-// Reset the guessed word
-// for (var i = 0, j = wordToMatch.length; i < j; i++) {
-// Put a space instead of an underscore between multi word "words"
-// if (wordToMatch[i] === " ") {
-// guessingWord.push(" ")
-// } else {
-// guessingWord.push("_")
-// }
-// }
-// updateDisplay();
-// }
-//Need function to update display.
-function updateDisplay() {
-    // document.getElementById("totalWins").innerText = wins;
-    // document.getElementById("currentWord").innerText = guessingWord;
-    // document.getElementById("remainingGuesses").innerText = numGuess;
-    // document.getElementById("guessedLetters").innerText = guessedLetters.join(" ");
 }
-updateDisplay();
